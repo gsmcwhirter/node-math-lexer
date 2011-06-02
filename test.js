69711d8728b44e8738d73db28cd9ed2a6d251676
Gregory McWhirter <greg@hallofkvasir.org>
2011-06-02 12:57:22 -0700

    This has a failing test at the moment

--- test.js
+++ test.js
@@ -401,22 +401,27 @@ exports["negative-numbers"] = function (test){
     var test1 = function (){return mlexer.parseString("-1")};
     var test2 = function (){return mlexer.parseString("-(a + 3)")};
     var test3 = function (){return mlexer.parseString("(-a + 3)")};
+    var test4 = function (){return mlexer.parseString("2 * b + -0.5 * a")};
 
     test.doesNotThrow(test1, mlexer.ParseError);
     test.doesNotThrow(test2, mlexer.ParseError);
     test.doesNotThrow(test3, mlexer.ParseError);
+    test.doesNotThrow(test4, mlexer.ParseError);
 
     test.equals(typeof test1(), "function", "test1");
     test.equals(typeof test2(), "function", "test2");
     test.equals(typeof test3(), "function", "test3");
+    test.equals(typeof test4(), "function", "test4");
 
     test["throws"](function(){test1()()}, mlexer.CalculationError, "test1");
     test["throws"](function(){test2()()}, mlexer.CalculationError, "test2");
     test["throws"](function(){test3()()}, mlexer.CalculationError, "test3");
+    test["throws"](function(){test4()()}, mlexer.CalculationError, "test4");
 
     test.equals(test1()({a: 1}), -1, "test1");
     test.equals(test2()({a: 1}), -4, "test2");
     test.equals(test3()({a: 1}), 2, "test3");
+    test.equals(test4()({a: 2, b: 1}), 1, "test4");
 
     test.done();
 }
